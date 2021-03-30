217c3c1c37cf1f9f49694e78947dca264482aee2
Mohammed-Khzami <68208071+Mohammed-Khzami@users.noreply.github.com>
2021-03-30 16:39:17 +0100

    connect to server

--- storage/connect.php
+++ storage/connect.php
@@ -1,7 +1,11 @@
 <?php 
-$host="localhost";
-$user="root";
-$password="";
-$dbname="Safimarket";
+// $host="localhost";
+// $user="root";
+// $password="";
+// $dbname="Safimarket";
+$host="remotemysql.com";
+$user="1DKFNNOy5Y";
+$password="HiHPjxNxbG";
+$dbname="1DKFNNOy5Y";
 $connect=mysqli_connect($host,$user,$password,$dbname);
 
