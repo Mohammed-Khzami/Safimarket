7371b82800295d7d0d345d1ed47a02f9ce821e93
Mohammed-Khzami <68208071+Mohammed-Khzami@users.noreply.github.com>
2021-03-30 15:54:53 +0100

    delete

--- storage/connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host="g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user="fb3tcnlz4w4lqd0j	";
-$password="xhl14d9wji3jvhqf";
-
-
-$dbname="o4wyk56dou6159lx";
-$connect=mysqli_connect($host,$user,$password,$dbname);
-
