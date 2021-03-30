e36c8d457b1f39418aac8ff70554339f27faa276
Mohammed-Khzami <68208071+Mohammed-Khzami@users.noreply.github.com>
2021-03-30 02:57:26 +0100

    Update connect.php
    hhcdhhcd

--- storage/connect.php
+++ storage/connect.php
@@ -1,8 +1,9 @@
 <?php 
-$host="epiz_28257815_safimarket";
-$user="epiz_28257815";
-$password="dtsNgY4QsRM8NxE";
+$host="g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user="fb3tcnlz4w4lqd0j	";
+$password="xhl14d9wji3jvhqf";
 
-$dbname="epiz_28257815_safimarket";
+
+$dbname="o4wyk56dou6159lx";
 $connect=mysqli_connect($host,$user,$password,$dbname);
 
